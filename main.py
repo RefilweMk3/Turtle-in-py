6d0b0bb2f7fc369b421a861e7587ffb527da1e12
RefilweMk3 <refilwekgwadi13@gmail.com>
2024-09-06 17:43:31 +0200

    Python Turtle

---
+++ main.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.Screen().bgcolor("yellow")
+
+turtle.title("Square on a canvas")
+b=turtle.Turtle()
+b.forward(100)
+b.right(90)
+b.forward(100)
+b.right(90)
+b.forward(100)
+b.right(90)
+b.forward(100)
+turtle.done()
